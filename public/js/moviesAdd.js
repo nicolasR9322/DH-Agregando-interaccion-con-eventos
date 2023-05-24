59379d93f40ac390fdab004497c67e1dae11a3cb
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-05-24 16:13:23 -0300

    desafio 2

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -2,6 +2,8 @@ window.onload = function(){
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
     let article = document.querySelector('article');
+    let inputTitulo = document.querySelector("#titulo");
+
     titulo.innerHTML = 'AGREGAR PELÍCULA';
     titulo.classList.add('titulo');
     article.classList.add('fondoTransparente');
@@ -11,4 +13,48 @@ window.onload = function(){
         titulo.style.color = "red"
     });
 
+    
+    console.log(inputTitulo)
+    let estadoSecreto = 0;
+    inputTitulo.addEventListener("keypress", (e) => {
+        let key = e.key;
+        console.log(key)
+        
+        switch (estadoSecreto) {
+                case 0:
+                (estadoSecreto == 0 && key == "s") ? estadoSecreto = 1 : "";
+                    console.log(estadoSecreto)
+                    break;
+                case 1:
+                    (estadoSecreto == 1 && key == "e") ? estadoSecreto = 2 : "";
+                    console.log(estadoSecreto)
+                    break;
+                case 2:
+                    (estadoSecreto == 2 && key == "c") ? estadoSecreto = 3: "";
+                    console.log(estadoSecreto)
+                    break;
+                case 3:
+                    (estadoSecreto == 3 && key == "r") ? estadoSecreto = 4 : "";
+                    console.log(estadoSecreto)
+                    break;
+                case 4:
+                    (estadoSecreto == 4 && key == "e") ? estadoSecreto = 5: "";
+                    console.log(estadoSecreto)
+                    break;
+                case 5:
+                    (estadoSecreto == 5 && key == "t") ? estadoSecreto = 6: "";
+                    console.log(estadoSecreto)
+                    break;
+                case 6:
+                    (estadoSecreto == 6 && key == "o") ? alert("Secreto Magico") : "";
+                    estadoSecreto = 0
+                    console.log(estadoSecreto)
+                    break;
+                default:
+                    console.log("no funciona")
+                    break;
+            }
+            
+        
+    })
 }
--- readme.md
+++ readme.md
@@ -12,3 +12,30 @@ descripcion: El objetivo será añadir nuevas caracteristicas al sitio web usand
         -en index.js agregar un evento que permita ocultar el menu lateral cuando el mouse deje el area del menu
 
         - en moviesList.ejs modificar el practico de la clase anterior para que el modo oscuro se aplique si el usuario pasa el mouse sobre el logo de digital house
+
+        - en moviesAdd.js modificar el titulo para que cambie de color cuando el mouse por encima
+    
+    desafio 2:
+        -en moviesAdd.ejs debemos crear una maquina de destados, debemos detectar cuando el usuario ingrese la palabra "secreto", en el input para ingresar el titulo de la pelicula
+
+        -debemos implementar una variable estadoSecreto que empiece con el numero 0 siguiendo el siguiente patron
+                ● 0 significa que todavía no escribió nada.
+                ● 1 significa que escribió “s”.
+                ● 2 significa que escribió “se”.
+                ● 3 significa que escribió “sec”.
+                ● 4 significa que escribió “secr”.
+                ● 5 significa que escribió “secre”.
+                ● 6 significa que escribió “secret”.
+        
+        -teniendo esto en cuenta implementaremos un evento que al presiona una tecla implemente la siguiente logica 
+
+        1. Si el estado es 0 y se presiona la tecla S, la variable estadoSecreto pasa a 1.
+        2. Si el estado es 1 y se presiona la tecla E, la variable estadoSecreto pasa a 2.
+        3. Si el estado es 2 y se presiona la tecla C, la variable estadoSecreto pasa a 3.
+        4. Si el estado es 3 y se presiona la tecla R, la variable estadoSecreto pasa a 4.
+        5. Si el estado es 4 y se presiona la tecla E, la variable estadoSecreto pasa a 5.
+        6. Si el estado es 5 y se presiona la tecla T, la variable estadoSecreto pasa a 6.
+        7. Si el estado es 6 y se presiona la tecla O, la variable estadoSecreto vuelve a 0 y se
+        dispara una alerta que diga “SECRETO MAGICO”.
+        8. Si no se cumple ninguna de las condiciones, el estado vuelve a 0.
+
